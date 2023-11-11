f53d191b180a19899bc383160b0b437267e5f889
CThinhIT <thinhtcit03@gmail.com>
2023-11-11 13:29:02 +0700

    no message

--- JS/product.js
+++ JS/product.js
@@ -8,47 +8,47 @@ var product = [
     },
     {
         "name": "Hoa hồng khô",
-        "id": "1",
+        "id": "2",
         "price": 150,
         "pic": "../ASSETS/img/Driedflower/h2.jpg",
         "type": 1
     },
     {
         "name": "Hoa sen khô<",
-        "id": "1",
+        "id": "3",
         "price": 95,
         "pic": "../ASSETS/img/Driedflower/h3.jpg",
         "type": 1
     },
     {
         "name": "Hoa cúc glixia khô",
-        "id": "1",
+        "id": "4",
         "price": 160,
         "pic": "../ASSETS/img/Driedflower/h4.jpg",
         "type": 1
     },
     {
         "name": "Hoa baby khô",
-        "id": "1",
+        "id": "5",
         "price": 135,
         "pic": "../ASSETS/img/Driedflower/h5.jpg",
         "type": 1
     },
     {
         "name": "Hoa bông gòn khô",
-        "id": "1",
+        "id": "6",
         "price": 50,
         "pic": "../ASSETS/img/Driedflower/h6.jpg",
         "type": 2
     }, {
         "name": "Hoa lúa mạch khô",
-        "id": "1",
+        "id": "7",
         "price": 70,
         "pic": "../ASSETS/img/Driedflower/h7.jpg",
         "type": 2
     }, {
         "name": "Hoa cúc nút áo khô",
-        "id": "1",
+        "id": "8",
         "price": 85,
         "pic": "../ASSETS/img/Driedflower/h8.jpg",
         "type": 2
@@ -82,7 +82,9 @@ function showProduct(product) {
                 break;
         }
         if (res1 == v.type) {
-            x += `<div class="col-3">
+            x += `
+            <div class="col-3">
+            <a href="detail.html?name=${v.id}">
                 <div class="thumbnail">
                     <img src="${v.pic}" alt="" class="imgcrop">
                     <div class="card-body">
@@ -91,7 +93,9 @@ function showProduct(product) {
                         <input type="button" class="btn btn-outline-danger" value="Mua ngay"></input>
                     </div>
                 </div>
-            </div>`;
+                </a>
+            </div>`
+        ;
         }
     });
     $("#title").html(title);
@@ -100,7 +104,7 @@ function showProduct(product) {
 
 showProduct(product);
 
-function searchProduct(filter) {
+function searchProduct() {
     let x = ``;
     let title = ``;
     var input, filter;
@@ -116,7 +120,9 @@ function searchProduct(filter) {
 
 
         if (v.name.toUpperCase().includes(filter)) {
-            x += `<div class="col-3">
+            x += `
+            <div class="col-3">
+            <a href="detail.html?name=${v.id}">
                 <div class="thumbnail">
                     <img src="${v.pic}" alt="" class="imgcrop">
                     <div class="card-body">
@@ -124,10 +130,25 @@ function searchProduct(filter) {
                         <p>${v.price}.000₫/100g</p>
                         <input type="button" class="btn btn-outline-danger" value="Mua ngay"></input>
                     </div>
-                </div>
+                </div> 
+                </a>
             </div>`;
         }
     });
     $("#title").html(title);
     $("#show-product").html(x);
 }
+
+function showProductDetail(product) {
+    let x = ``;
+    var urlID = window.location.href;
+    res1 = urlID.slice(-1);
+    $.each(product, function (k, v) {
+        if (v.id == res1) {
+            x += `<div class="col-5"><img src="${v.pic}" alt="" class="imgcrop" width="100%"></div>
+            <div class="col-7">${v.name}</div>`;
+        }
+    });
+    $("#detail").html(x);
+}
+showProductDetail(product);
